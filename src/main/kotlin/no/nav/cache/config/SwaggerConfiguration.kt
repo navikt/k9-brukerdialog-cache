eda78cfa5554e3aceb5e8a2e7b6af8844607819f
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2022-04-06 11:40:23 +0200

    Legger på støtte for authorization header på swagger.

--- src/main/kotlin/no/nav/cache/config/SwaggerConfiguration.kt
+++ src/main/kotlin/no/nav/cache/config/SwaggerConfiguration.kt
@@ -1,8 +1,11 @@
 package no.nav.cache.config
 
+import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.ExternalDocumentation
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
 import io.swagger.v3.oas.models.servers.Server
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -29,5 +32,17 @@ class SwaggerConfiguration {
                     .description("K9 Brukerdialog Cache GitHub repository")
                     .url("https://github.com/navikt/k9-brukerdialog-cache")
             )
+            .components(
+                Components()
+                    .addSecuritySchemes(
+                        "bearer-jwt", SecurityScheme()
+                            .type(SecurityScheme.Type.HTTP)
+                            .scheme("bearer")
+                            .bearerFormat("JWT")
+                            .`in`(SecurityScheme.In.HEADER)
+                            .name("Authorization")
+                    )
+            )
+            .addSecurityItem(SecurityRequirement().addList("bearer-jwt", listOf("read", "write")))
     }
 }
