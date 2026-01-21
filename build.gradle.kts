75c9ff355b69b66cc3dcb5129ac09a568be74c02
Qadeer Khan <qadeer.ahmad.khan@nav.no>
2026-01-21 15:46:05 +0100

    upgrade gradle + downgrade kotlin + upgrade spring boot

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,10 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "2.3.0"
-    kotlin("plugin.spring") version "2.3.0"
-    kotlin("plugin.jpa") version "2.3.0"
-    id("org.springframework.boot") version "4.0.0"
+    kotlin("jvm") version "2.2.10"
+    kotlin("plugin.spring") version "2.2.10"
+    kotlin("plugin.jpa") version "2.2.10"
+    id("org.springframework.boot") version "4.0.1"
     id("io.spring.dependency-management") version "1.1.7"
     id("org.sonarqube") version "7.2.2.6593"
     jacoco
